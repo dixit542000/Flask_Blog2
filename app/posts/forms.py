88695a951f802dcc32493f12a6269dccbfbe0f7e
dixit542000 <dixit.bda1710@ict.gnu.ac.in>
2019-05-30 18:20:36 +0530

    before branching

--- app/posts/forms.py
+++ app/posts/forms.py
@@ -7,3 +7,7 @@ class PostForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
     content = TextAreaField('Content', validators=[DataRequired()])
     submit = SubmitField('Post')
+
+class AddCommentForm(FlaskForm):
+    body = StringField("Body", validators=[DataRequired()])
+    submit = SubmitField("Post")
--- app/posts/routes.py
+++ app/posts/routes.py
@@ -57,3 +57,19 @@ def delete_post(post_id):
     db.session.commit()
     flash('Your post has been deleted!', 'success')
     return redirect(url_for('main.home'))
+
+
+@posts.route("/post/<int:post_id>/comment", methods=["GET", "POST"])
+@login_required
+def comment_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    form = AddCommentForm()
+    if request.method == 'POST': # this only gets executed when the form is submitted and not when the page loads
+        if form.validate_on_submit():
+            comment = Comment(body=form.body.data, article=post.id)
+            db.session.add(comment)
+            db.session.commit()
+            flash("Your comment has been added to the post", "success")
+            return redirect(url_for("post", post_id=post.id))
+    return render_template("comment_post.html", title="Comment Post",
+form=form, post_id=post_id)
