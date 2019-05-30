e94dd422ea50601f2613f5700b371350770ceda1
dixit542000 <dixit.bda1710@ict.gnu.ac.in>
2019-05-30 18:31:57 +0530

    base

--- app/posts/routes.py
+++ app/posts/routes.py
@@ -68,8 +68,7 @@ def comment_post(post_id):
         if form.validate_on_submit():
             comment = Comment(body=form.body.data, article=post.id)
             db.session.add(comment)
-            db.session.commit()
-            #hekko
+            db.session.commit(
             flash("Your comment has been added to the post", "success")
             return redirect(url_for("post", post_id=post.id))
     return render_template("comment_post.html", title="Comment Post",
