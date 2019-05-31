2299dd81f191315aa00370169f7a792b1f528aeb
dixit542000 <dixit.bda1710@ict.gnu.ac.in>
2019-05-31 12:44:10 +0530

    base

--- app/posts/routes.py
+++ app/posts/routes.py
@@ -1,8 +1,8 @@
 from flask import render_template, url_for, flash, redirect, request, abort, Blueprint
 from flask_login import current_user, login_required
 from app import db
-from app.models import Post
-from app.posts.forms import PostForm
+from app.models import Post,Comment
+from app.posts.forms import PostForm, AddCommentForm
 
 posts = Blueprint('posts', __name__)
 
@@ -57,3 +57,19 @@ def delete_post(post_id):
     db.session.commit()
     flash('Your post has been deleted!', 'success')
     return redirect(url_for('main.home'))
+
+
+@posts.route("/post/<int:post_id>/comment", methods=["GET", "POST"])
+@login_required
+def comment_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    form = AddCommentForm()
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            comment = Comment(body=form.body.data, post_id=post.id)
+            db.session.add(comment)
+            db.session.commit()
+            flash("Your comment has been added to the post", "success")
+            return redirect(url_for("posts.post", post_id=post.id))
+    return render_template("comment_post.html", title="Comment Post",
+form=form, post_id=post_id)
